d9d32bf817429037b00caa95d57e710adaed468f
Yasin Çalışkan <yasincaliskan26@yahoo.com>
2020-05-12 16:48:11 +0300

    Fixed Card Item

--- NativeJS/data.js
+++ NativeJS/data.js
@@ -15,9 +15,7 @@ $.getJSON(file, function(data) {
     cardItem += "<div class='new'><small> NEW </small></div>";
     }
     cardItem += "</div><div class='pad15'>";
-
     cardItem += "<img src='"+data[i].imageS+"'>";
-    // cardItem += "<div style='background:url("+data[i].imageS+") no-repeat;width:200px;height:200px;padding-left:30px;'><i class='far fa-heart'>"+data[i].params.likeCount+"</i></div>";
     cardItem += "<p><b>"+data[i].name+"</b></p>";
     cardItem += "<p>"+data[i].params.rebsorte+"</p>";
     cardItem += "<p><b>"+data[i].priceText+ "</b> <small><del> "+data[i].oldPriceText+"</del></small></p>";
